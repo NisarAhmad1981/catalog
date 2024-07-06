a801471576569eb8f7f0276d4f398cbb2ad189c0
NisarAhmad1981 <30634781+NisarAhmad1981@users.noreply.github.com>
2024-07-06 22:02:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("Zena's Amazing Athleisure Catalog")
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-
+session = get_active_session()
 # get a list of colors for a drop list selection
 table_colors = session.sql("select color_or_style from catalog_for_website")
 pd_colors = table_colors.to_pandas()
